a1d360bfcdf7b1c7eca4798606cc1b15d1e9ed0e
Om Bhatt <72290164+Debug756@users.noreply.github.com>
2022-02-07 12:24:59 +0530

    mongodb fix

--- src/db/conn.js
+++ src/db/conn.js
@@ -3,19 +3,7 @@ const mongoose = require("mongoose");
 const { MongoGridFSChunkError } = require("mongoose/node_modules/mongodb");
 
 // creating a database 
-// mongoose.connect("mongodb://localhost:27017/OmDynamic", {
-//     // useCreateIndex:true,
-//     useNewUrlParser:true,
-//     useUnifiedTopology:true
-// }).then(() => {
-//     console.log("connection successful");
-// }).catch((error) => {
-//     console.log(error);
-// })
-
-
-mongoose.connect("mongodb://future-x.herokuapp.com:27017/OmDynamic" ,
-{
+mongoose.connect("mongodb://future-x.herokuapp.com/OmDynamic", {
     // useCreateIndex:true,
     useNewUrlParser:true,
     useUnifiedTopology:true
@@ -24,4 +12,3 @@ mongoose.connect("mongodb://future-x.herokuapp.com:27017/OmDynamic" ,
 }).catch((error) => {
     console.log(error);
 })
-
